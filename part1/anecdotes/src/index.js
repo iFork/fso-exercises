7ed74dd6808af3474f896cdbcc176d22803a5cff
Mher <tools@logreg.e4ward.com>
2020-07-17 22:32:44 +0400

    CHORE(Part 1: Exercises 1.12-1.14): Clean
    Remove comments for further exploration from 'Anecdotes' exercise,
    keep them in 'practice' branch.

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -80,7 +80,8 @@ const App = ({anecdotes}) =>
 {
     const len = anecdotes.length;
 
-    let randNum = (() => { console.log("randNum initializer called"); return getRandomNum(len);})();
+    let randNum = (() => { console.log("randNum initializer called");
+                            return getRandomNum(len);})();
     const [selected, setSelected] = useState( (() => {
                                     console.log("useState() called");
                                     return randNum})() );
@@ -97,38 +98,6 @@ const App = ({anecdotes}) =>
         newVotes[selected] += 1;
         setVotes(newVotes);
     }
-
-
-	// RE-RENDER ISSUE related to the vlaue passed to setStatus().
-    // TODO: ToEXPLAIN: NOTE: App does NOT re-render when same (as in state initializer)
-    // number is used in click handler setSelected.
-    // ToEXPLAIN: NOTE: Also having randNumDummy = randNum + 1 causes re-render but after multiple 
-    // clicks it stops to cause re-render. 
-    // Check: shouldComponentUpdate(nextProp, nextState) or forceUpdate()
-    //
-            // <Button handleClick={() => setSelected(randNum)}
-	// or
-    // let randNumDummy = randNum + 1;
-    // console.log("randNumDummy is", randNumDummy);
-            // <Button handleClick={() => {
-                // console.log("Click handler called");
-                // setSelected(randNum);
-                // }
-            // }
-            // text="Next" />
-
-	/* console log - one can see no re-render occures at the last line of log:
-		randNum initializer called
-		Random number is:  1 , max is:  6
-		useState() called
-		randNumDummy is 2
-		Click handler called
-		randNum initializer called
-		Random number is:  4 , max is:  6
-		useState() called
-		randNumDummy is 5
-	(33)    Click handler called  //<-- no other calls get logged
-	*/
 
     return (
         <div>
