bf613835d7201be0c2ab58cfd2e0b1331396fddd
Mher <tools@logreg.e4ward.com>
2020-07-25 15:17:55 +0400

    ✨ Part 2c: Exercise 2.11
    Modify 'phonebook' app to use
    - json-server to serve data,
    - 'axios' lib for getting a promise of fetching,
    - useEffect() hook to fetch data after render and set state.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 
 const PersonForm = ({newName, newNumber, setNewName, setNewNumber, 
@@ -61,15 +62,20 @@ const isDuplicate = (name, persons) => {
 
 
 function App() {
-    const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-    ]);
+    const [persons, setPersons] = useState([]);
     const [newName, setNewName] = useState("");
     const [newNumber, setNewNumber] = useState("")
     const [filterName, setFilterName] = useState("")
+
+    const fetchHook = () => {
+       axios
+            .get("http://localhost:3001/persons")
+            .then(response => {
+                console.log("fulfilled, response is:", response);
+                setPersons(response.data);
+            })
+    }
+    useEffect(fetchHook, [])
 
     const personsToShow = filterName 
                           ? persons.filter(person =>
