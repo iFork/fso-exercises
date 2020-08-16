c967f21168276159625cd20a09b5ba89161bd16b
Mher <tools@logreg.e4ward.com>
2020-08-16 17:15:39 +0400

    Practice: Part 3: Copy frontend build/ from part 2

---
+++ part3/practice/build/precache-manifest.0b028aa30faa1562b02f34220ca1e91d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fe62f20f9063e9c76687037a0a76720",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c177847b69c47b11f3d3",
+    "url": "/static/css/main.dff47a8e.chunk.css"
+  },
+  {
+    "revision": "61ceed379e8967e2eb84",
+    "url": "/static/js/2.ce10faeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ce10faeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c177847b69c47b11f3d3",
+    "url": "/static/js/main.fc2d912b.chunk.js"
+  },
+  {
+    "revision": "de9d7b2868e288e79b25",
+    "url": "/static/js/runtime-main.bb9ee77d.js"
+  }
+]);
