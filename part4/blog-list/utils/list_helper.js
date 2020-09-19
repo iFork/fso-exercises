9325dfd96f7888d15df7690432dd35c635ef33fa
Mher <tools@logreg.e4ward.com>
2020-09-20 01:30:02 +0400

    ✨ Part 4a: Ex. 4.5 - Favorite blog, tested
    Add favoriteBlog() func to get the blog with most likes and
    its tests in a describe() block.

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -9,8 +9,25 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reducer, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  return blogs.reduce( (favorite, blog) => {
+    // console.log('favorite is:', favorite, 'current blog is:', blog);
+    const favLikes  = favorite.likes || 0;
+    const blogLikes = blog.likes     || 0;
+    // console.log('favLikes', favLikes, 'blogLikes', blogLikes);
+    if ( blogLikes > favLikes ) {
+      return favorite = (({ title, author, likes }) => 
+          ({ title, author, likes }))(blog);
+    } else {
+      return favorite;
+    }
+  }, {} ); // may put 1st elem for optimizing
+  // 1 > undefined // false
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
 
