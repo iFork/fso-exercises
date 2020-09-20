f83df179161bfbf696b8261d14d1b47bd551d2fc
Mher <tools@logreg.e4ward.com>
2020-09-20 22:07:05 +0400

    Practice: Part 4b - Set up test environment
    Prefix npm scripts with NODE_ENV environment variable with production,
    development, test values, respectively.
    (Add connection string to test database in .env file).
    Update utils/config module to set MONGODB_URI value depending on
    NODE_ENV value.

--- part3/practice/utils/config.js
+++ part3/practice/utils/config.js
@@ -2,5 +2,10 @@
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
-const { MONGODB_URI } = process.env;
+let { MONGODB_URI } = process.env;
+
+if (process.env.NODE_ENV === 'test') {
+    MONGODB_URI = process.env.TEST_MONGODB_URI;
+}
+
 module.exports = { PORT, MONGODB_URI };
