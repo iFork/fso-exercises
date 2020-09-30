d6dfd6501793a80b5aa74aaa5327cd6c8bfe1a61
Mher <tools@logreg.e4ward.com>
2020-10-01 00:24:13 +0400

    ✨ Part 4b: Ex. 4.8 - Set up test environment
    Prefix npm scripts with NODE_ENV env variables.
    Change utils/config module to honor run mode (NODE_ENV) by setting
    mongodb connection strings accordingly.
    (Add connection string to test db in .env)

--- part4/blog-list/utils/config.js
+++ part4/blog-list/utils/config.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 
-const MONGODB_URI = process.env.MONGODB_URI;
-const PORT = process.env.PORT;
+let { MONGODB_URI } = process.env;
+if (process.env.NODE_ENV !== 'production') {
+  MONGODB_URI = process.env.TEST_MONGODB_URI;
+}
+const { PORT } = process.env;
 
 module.exports = { MONGODB_URI, PORT };
