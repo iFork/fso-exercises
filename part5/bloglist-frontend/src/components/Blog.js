44d12406e0021f6a1b4dc00a39ef602a7d225633
Mher <tools@logreg.e4ward.com>
2020-11-03 23:51:16 +0400

    ✨ Part 5b: Ex. 5.7 Show/hide blog details
    [components/Blog]
    Add state to track view mode - detailed or compact.
    Add a button to toggle view mode.
    Render conditionally (not css 'display:none' trick) either detailed or
    compact jsx.
    Add inline style for a blog.

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,56 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  console.log({ blog });
+  const [viewDetailed, setViewDetailed] = useState(false)
+
+  const toggleViewDetailed = () => {
+    setViewDetailed((prevState) => !prevState)
+  }
+
+  const blogStyle = {
+    border: "solid",
+    borderWidth: 1,
+    paddingTop: 10,
+    paddingLeft: 2,
+    paddingBottom: 3,
+    marginBottom: 5,
+  }
+
+  const compactView = (
+    <div>
+      {blog.title} {blog.author}
+      <button
+        type="button"
+        onClick={()=> toggleViewDetailed()}
+      >
+        View
+      </button>
+    </div>
+    );
+
+  const detailedView = (
+    <div>
+      {blog.title} {blog.author}
+      <button
+        type="button"
+        onClick={()=> toggleViewDetailed()}
+      >
+        Hide
+      </button>
+      <div>{blog.url}</div>
+      <div>likes {blog.likes}
+        <button>Like</button>
+      </div>
+      <div>{blog.user ? blog.user.name : ""}</div>
+    </div>
+  )
+  return (
+    <div style={blogStyle}>
+      { viewDetailed || compactView }
+      { viewDetailed && detailedView }
+    </div>
+  )
+}
 
 export default Blog
