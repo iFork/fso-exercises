e233973ad07f2036b7dd7c6577cd73f9865fac05
Mher <tools@logreg.e4ward.com>
2021-01-13 14:05:54 +0400

    ✨ Part 5d: Ex. 5.17 E2E test login form
    [cypress/integration/bloglist-app.spec]
    Reset db in beforeEach hook using api for testing.
    Test that login form is shown.

--- part5/bloglist-frontend/cypress/integration/bloglist-app.spec.js
+++ part5/bloglist-frontend/cypress/integration/bloglist-app.spec.js
@@ -1,8 +1,12 @@
 describe('Bloglist app', function () {
   beforeEach(function () {
+    // clear db with api for testing
+    cy.request('POST', '/api/testing/reset');
     cy.visit('/');
   });
-  it('Opens page', function () {
-    cy.contains(/log in to application/i);
+  it('Login form is shown', function () {
+    cy.contains(/username/i);
+    cy.contains(/password/i);
+    cy.contains(/login/i);
   });
 });
