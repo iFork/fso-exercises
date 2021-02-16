7e9d1050cbbf1cd9ebed88876e38f6b479a5a322
Mher <tools@logreg.e4ward.com>
2021-02-16 16:14:28 +0400

    ✨ Part 6b: Ex. 6.9 redux-devtools-extension
    [Dependency]
    Add redux-devtools-extension as dev dependency.
    
    [store]
    Pass composeWithDevTools() of redux-devtools-extension as enhancer to
    createStore() in place of window.__REDUX_DEVTOOLS_EXTENSION__().

--- part6/redux-anecdotes/src/store.js
+++ part6/redux-anecdotes/src/store.js
@@ -1,8 +1,10 @@
 import { createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import reducer from './reducers/anecdoteReducer'
 
 export default createStore(
   reducer,
-  typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION__ 
-    && window.__REDUX_DEVTOOLS_EXTENSION__()
+  // typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION__ 
+    // && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeWithDevTools()
 )
