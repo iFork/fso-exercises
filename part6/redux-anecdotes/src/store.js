1eac25db18a366aaa48472c62969db44cf07aad8
Mher <tools@logreg.e4ward.com>
2021-02-16 16:09:12 +0400

    ✨ Part 6b: Ex. 6.9 move store into its file

---
+++ part6/redux-anecdotes/src/store.js
@@ -0,0 +1,8 @@
+import { createStore } from 'redux'
+import reducer from './reducers/anecdoteReducer'
+
+export default createStore(
+  reducer,
+  typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION__ 
+    && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
