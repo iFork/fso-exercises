40c0b746828ee54c798994f14e3cbf7b6ce0b86d
Mher <tools@logreg.e4ward.com>
2021-02-20 13:39:25 +0400

    Practice: Part 6c separate store into its module

--- part6/practice/src/index.js
+++ part6/practice/src/index.js
@@ -1,29 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import App from './App';
-import noteReducer from './reducers/noteReducer';
-import filterReducer from './reducers/filterReducer';
+import store from './store';
 
 // Steps:
 // wire up json-server (bd.json, npm script)
 // install axios
 // add data fetching/API-communication services under services folder, use axios
-
-const reducers = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer
-})
-// create store with a reducer callback
-const store = createStore(
-  reducers,
-  // connect to Redux Dev Tools (extension of Chrome)
-  // typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION__ 
-    // && window.__REDUX_DEVTOOLS_EXTENSION__()
-  composeWithDevTools()
-);
 
 
 // NOTE: wrapping render in renderApp() to pass a callback to store.subscribe() 
---
+++ part6/practice/src/store.js
@@ -0,0 +1,19 @@
+import { createStore, combineReducers } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import noteReducer from './reducers/noteReducer';
+import filterReducer from './reducers/filterReducer';
+
+const reducers = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer
+})
+// create store with a reducer callback
+const store = createStore(
+  reducers,
+  // connect to Redux Dev Tools (extension of Chrome)
+  // typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION__ 
+    // && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeWithDevTools()
+)
+
+export default store
