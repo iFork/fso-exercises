4158f3ac18e4c62061caccac6ad3e0ce5068b899
Mher <tools@logreg.e4ward.com>
2021-02-22 17:31:50 +0400

    Practice: Part 6d refactor Notes w/ redux connect
    Refactor <Notes /> to hook it up w/ redux store with
    connect(mapStateToProps, mapDispatchToProps)() instead of redux hooks
    (useSelector and useDispatch).

--- part6/practice/src/Notes.js
+++ part6/practice/src/Notes.js
@@ -1,34 +1,12 @@
-import { useSelector, useDispatch } from 'react-redux';
+import { connect } from 'react-redux';
 import { toggleImportance } from './reducers/noteReducer';
 import Note from './Note';
 import noteService from './services/noteService';
 
 
-function Notes () { 
-  // useSelector hook also subscribes component to the store (set in Provider),
-  // it is called when component renders (except when component reference remains
-  // invariant?), when action is dispatched to a store and it forces re-render
-  // if returned value is different from previous returned value (using strict
-  // `===` equality).
-  const notes = useSelector(({ notes, filter }) => {
-    // NOTE: Q: why this runs 2 times on filter change?
-    // A: (maybe) because it runs once an action is dispatched (in filter component
-    // setFilter) and checks if re-rendering is due and once after re-rendering (?)
-    //
-    // console.log({ filter });
-    // TODO: capitalize string constants of filter values?
-    if ( filter === 'all' )
-      return notes;
-    return filter === 'important'
-      // NOTE: pay attention that value of important is *boolean*, not string
-      ? notes.filter(note => note.important)
-      : notes.filter(note => !note.important);
-
-  });
+function Notes ({ notes, toggleImportance }) { 
   // TODO: here we pass inline callback with a dispatch to another component.
   // Do we need to use useCallback() or useMemo() to optimize out excessive renders?
-  const dispatch = useDispatch();
-
   async function handleImportanceToggling(note) {
     // /* const updatedNote = */ await noteService.toggleImportance(note)
     await noteService.updateNote({
@@ -36,10 +14,7 @@ function Notes () {
       important: !note.important
     });
     // TODO: catch errors
-    // TODO: Q: Should we ditch toggleImportance action in favor of more generic
-    // updateNote() action? 
-    // Like we did in noteService ?
-    dispatch(toggleImportance(note.id));
+    toggleImportance(note.id);
   }
   return ( 
     <ul>
@@ -61,4 +36,34 @@ function Notes () {
   );
 }
 
-export default Notes;
+// passing mapStateToProps to connect subscribes component to the store (set in Provider),
+// it is called whenever store has updated. 
+function mapStateToProps({ notes, filter }) {
+  // NOTE: note OBSOLETED after using connect() instead of useSelector() hook.
+  // Now (w/ connect()) it runs once on filter change.
+  // Q: why this runs 2 times on filter change?
+  // OBSOLETE A: (maybe) because it runs once an action is dispatched (in filter component
+  // setFilter) and checks if re-rendering is due and once after re-rendering (?)
+  // TODO: capitalize string constants of filter values?
+  if ( filter === 'all' )
+    return { notes };
+  return {
+    notes: (filter === 'important'
+      // NOTE: pay attention that value of important is *boolean*, not string
+      ?  notes.filter(note => note.important) 
+      :  notes.filter(note => !note.important)
+    )
+  };
+}
+
+// function mapDispatchToProps(dispatch) {
+//   return {
+//     toggleImportance: (id) => dispatch(toggleImportance(id)),
+//   };
+// }
+// or object shorthand form where redux  binds each action creator to
+// dispatch, like above, but does it internally
+const mapDispatchToProps = { toggleImportance };
+
+const ConnectedNotes = connect(mapStateToProps, mapDispatchToProps)(Notes);
+export default ConnectedNotes;
