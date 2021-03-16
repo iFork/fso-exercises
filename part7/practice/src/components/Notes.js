7b5ba02e23c4cf2dfed22b76640cb09d71f2b205
Mher <tools@logreg.e4ward.com>
2021-03-16 17:13:33 +0400

    Practice: Part 7c use bootstrap's <Table />

--- part7/practice/src/components/Notes.js
+++ part7/practice/src/components/Notes.js
@@ -1,16 +1,32 @@
 import { Link } from 'react-router-dom';
+import { Table } from 'react-bootstrap';
 
 export default function Notes ({ notes }) { 
-  return ( 
-    <ul>
-      {notes.map(note => {
-        return (
-          <li key={note.id}>
-            <Link to={`/notes/${note.id}`}>{note.content}</Link>
-          </li>
-        );
-      })}
-    </ul>
+  return (
+    <div> 
+      <h1>Notes</h1>
+      <Table striped hover >
+        <thead>
+          <tr>
+            <th>Note title</th>
+            <th>User</th>
+          </tr>
+        </thead>
+        <tbody>
+          {notes.map(note => {
+            return (
+              <tr key={note.id}>
+                <td>
+                  <Link to={`/notes/${note.id}`}>{note.content}</Link>
+                </td>
+                <td>
+                  {note.user}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table></div>
   );
 }
 
